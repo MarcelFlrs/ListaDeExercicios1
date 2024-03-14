e2d8a322e76221d866161584f813fd3cd137a3d8
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-14 10:48:30 -0300

    Exercício 02

---
+++ Exercicio02.java
@@ -0,0 +1,8 @@
+public class Exercicio02 {
+    public static void executar() {
+        int num1 = Prompt.lerInteiro("Digite o primeiro número: ");
+        int num2 = Prompt.lerInteiro("Digite o segundo número: ");
+        
+        System.out.println("A soma dos dois números digitados é: " + (num1+num2));
+    }
+}
