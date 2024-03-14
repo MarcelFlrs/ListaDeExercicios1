7f36b1caa55441612e1c3208d44c45ab1e16f9a4
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-14 10:50:57 -0300

    Exercício 08

---
+++ Exercicio08.java
@@ -0,0 +1,20 @@
+public class Exercicio08 {
+    public static void executar() {
+        int numero = Prompt.lerInteiro("Digite um número entre 1 e 5: ");
+
+        switch (numero) {
+            case 1: System.out.println("Um");
+                break;
+            case 2: System.out.println("Dois");
+                break;
+            case 3: System.out.println("Três");
+                break;
+            case 4: System.out.println("Quatro");
+                break;
+            case 5: System.out.println("Cinco");
+                break;
+            default: System.out.println("Número inválido!");
+                break;
+        }
+    }
+}
