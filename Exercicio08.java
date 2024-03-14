e32c5be1a35a64ea0fd99e824077e242e088f640
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-14 19:10:58 -0300

    Exercício 8

--- Exercicio08.java
+++ Exercicio08.java
@@ -1,20 +1,11 @@
 public class Exercicio08 {
     public static void executar() {
-        int numero = Prompt.lerInteiro("Digite um número entre 1 e 5: ");
+        int numero = Prompt.lerInteiro("Digite um número: ");
 
-        switch (numero) {
-            case 1: System.out.println("Um");
-                break;
-            case 2: System.out.println("Dois");
-                break;
-            case 3: System.out.println("Três");
-                break;
-            case 4: System.out.println("Quatro");
-                break;
-            case 5: System.out.println("Cinco");
-                break;
-            default: System.out.println("Número inválido!");
-                break;
+        if (numero >= 50) {
+            System.out.println("Este número é maior ou igual a 50");
+        }else{
+            System.out.println("Este número é menor do que 50");
         }
     }
 }
