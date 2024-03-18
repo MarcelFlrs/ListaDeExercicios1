42726ccbde605f3a52b55c1d2fbd52dc7cdad4d3
MarcelFlrs <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-18 11:21:30 -0300

    Exercício 13

--- Programa.java
+++ Programa.java
@@ -11,7 +11,14 @@ public class Programa {
         // Exercicio09.executar();
         // Exercicio10.executar();
         // Exercicio11.executar();
-        Exercicio12.executar();
+        // Exercicio12.executar();
+        // Exercicio13.executar();
+        // Exercicio14.executar();
+        // Exercicio15.executar();
+        // Exercicio16.executar();
+        // Exercicio17.executar();
+        Exercicio18.executar();
+
         
     }
 }
