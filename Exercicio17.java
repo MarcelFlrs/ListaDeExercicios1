474667ee36730e291b2712b037bf410b804458e1
Marcel <127535882+MarcelFlrs@users.noreply.github.com>
2024-03-18 19:14:19 -0300

    Exercício 17

--- Exercicio17.java
+++ Exercicio17.java
@@ -4,9 +4,9 @@ public class Exercicio17 {
         double nota2 = Prompt.lerDecimal("Digite sua segunda nota: ");
         double nota3 = Prompt.lerDecimal("Digite sua terceira nota: ");
 
-        int peso1 = 2;
-        int peso2 = 3;
-        int peso3 = 5;
+        int peso1 = Prompt.lerInteiro("Peso 1: ");
+        int peso2 = Prompt.lerInteiro("Peso 2: ");;
+        int peso3 = Prompt.lerInteiro("Peso 3: ");;
 
         double mediaP = (nota1*peso1 + nota2*peso2 + nota3*peso3) / (peso1 + peso2 + peso3);
         System.out.println("Média ponderada: " + mediaP);
